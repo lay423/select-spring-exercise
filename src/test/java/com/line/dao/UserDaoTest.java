71b9c5a57266bb6b2b350aa16cc2267926b900d4
lay423 <junpang486@gmail.com>
2022-10-20 11:47:14 +0900

    UserFactory

--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -4,22 +4,33 @@ import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
     @Test
     @DisplayName("insert와 select가 잘 작동되는지 test")
     public void insertAndSelect() throws SQLException {
 
         //UserDao userDao = new UserDao();
-        UserDao userDao = new UserDao();
-        User user = new User("15", "king3", "11223");
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        User user = new User("14", "king2", "11223");
         userDao.insert(user);
 
-        User selectUser = userDao.select("15");
-        Assertions.assertEquals("king3", selectUser.getName());
+        User selectUser = userDao.select("14");
+        Assertions.assertEquals("king2", selectUser.getName());
     }
 }
